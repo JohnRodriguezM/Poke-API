a3922ab088b7285073a654ea5d5f1d48039ff028
john jairo rodriguez medina <johnjairorodriguez384@gmail.com>
2022-04-05 22:14:10 -0500

    poke api end

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,8 +1,6 @@
 "use strict";
 
-import { 
-  registerImage 
-} from './lazyLoading.mjs'
+import { registerImage } from "./lazyLoading.mjs";
 
 console.log(registerImage);
 
@@ -30,34 +28,35 @@ const getData_forImg = async (dataDeData) => {
       let imgPokemonFront = datos.sprites.front_default;
       let imgPokemonBack = datos.sprites.back_default;
       let imgPokemonShiny = datos.sprites.front_shiny;
-      cards.innerHTML += `
-     <div class = "imgs" >
-      <li>${datos.name}</li>
+
+      let contenido = `
+      <div class = "imgs" >
+      <p style = "font-size: 20px">${
+        datos.name.charAt(0).toUpperCase() + datos.name.slice(1)
+      }</p>
         <div class="card">
-        <figure><img src="${imgPokemonFront}" alt=""></figure>
+        <figure >
+        <img src="${imgPokemonFront}" alt=""> 
+        </figure>
         <figure><img src="${imgPokemonBack}" alt=""></figure>
         <figure> <img src="${imgPokemonShiny}" alt=""></figure>
-        
-                     
             </div>
         </div>
-        `;
-
+      `;
+      cards.innerHTML += contenido;
+     
     }
-    d.addEventListener('click', e => {
-      if(e.target === btnOcultar){
-        let cards = [...d.querySelectorAll('.card')];
-        for(let el of cards){
-          el.style.display = 'none';
+    d.addEventListener("click", (e) => {
+      if (e.target === btnOcultar) {
+        let cards = [...d.querySelectorAll(".card")];
+        for (let el of cards) {
+          el.style.display = "none";
         }
-        getDataPokemon()
-        location.reload()
+        getDataPokemon();
+        location.reload();
       }
-    })
-  } 
-  
-  
-  catch (err) {
+    });
+  } catch (err) {
     console.log(err);
   }
 };
@@ -87,7 +86,7 @@ const getDataPokemon = async () => {
     // variable para guardar la data
     let res = data.results;
     console.log(res);
-    res.forEach((el,index) => {
+    res.forEach((el, index) => {
       cards.innerHTML += `
      <div class = "names">
      <li class = "liPokeName">Pokemón ${index + 1}:</li>
@@ -96,20 +95,24 @@ const getDataPokemon = async () => {
 
         `;
     });
+
     // se ejecuta funcion que me trae la data de las url de cada pokemon
-    d.addEventListener("click", e => {
-      if (e.target === btn) {
-        getDatasPokemonesResults(res);
-        let names = [...d.querySelectorAll(".names")];
-        console.log(names);
-        for (let el of names) {
-          el.style.display = "none";
-          
+    d.addEventListener(
+      "click",
+      (e) => {
+        if (e.target === btn) {
+          getDatasPokemonesResults(res);
+          let names = [...d.querySelectorAll(".names")];
+          console.log(names);
+          for (let el of names) {
+            el.style.display = "none";
+          }
         }
+      },
+      {
+        once: true,
       }
-    },{
-      once: true,
-    });
+    );
   } catch (err) {
     console.log(err);
   }
@@ -117,7 +120,8 @@ const getDataPokemon = async () => {
 
 // activador de evento en el navegador
 
-
-d.addEventListener("DOMContentLoaded", getDataPokemon,{
+d.addEventListener("DOMContentLoaded", getDataPokemon, {
   once: true,
 });
+
+
